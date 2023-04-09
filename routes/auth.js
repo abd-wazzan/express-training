383248d0cf0d76c8da32e13a5a82e7938bb28304
Abdulrahman Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-04-09 14:00:26 +0400

    Write login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 const router = express.Router();
@@ -25,5 +26,38 @@ router.post("/signup", (req, res, next) => {
 
 });
 
-
+router.post("/login", (req, res, next) => {
+    let fetchedUser;
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Auth failed',
+                })
+            }
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: 'Auth failed',
+                })
+            }
+            const token = jwt.sign(
+                {email: fetchedUser.email, userId: fetchedUser._id},
+                'secret_key',
+                {expiresIn: '1h'}
+            );
+            res.status(200).json({
+                message: 'Authenticated!',
+                token: token
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err
+            });
+        });
+});
 module.exports = router;
